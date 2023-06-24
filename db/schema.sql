21fbca30ee636e381c5d024dac64768f29f792ca
Nick <chargerfan428@aol.com>
2023-06-24 13:25:42 -0700

    updated employee table to meet acceptance criteria

--- db/schema.sql
+++ db/schema.sql
@@ -8,21 +8,24 @@ DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     department_id INT NOT NULL,
-    department_name VARCHAR(20) NOT NULL,
+    department_name VARCHAR(20) NOT NULL
 );
 
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE employees (
     employee_id INT NOT NULL,
-    employee_name VARCHAR(20) NOT NULL,
+    employee_first_name VARCHAR(20) NOT NULL,
+    employee_last_name VARCHAR(20) NOT NULL,
+    employee_title VARCHAR(20) NOT NULL,
     department_id INT NOT NULL,
     salary INT NOT NULL,
+    manager_name VARCHAR(20) NOT NULL
 );
 
 DROP TABLE IF EXISTS roles;
 
 CREATE TABLE roles (
     role_id INT NOT NULL,
-    role_name VARCHAR(20) NOT NULL,
+    role_name VARCHAR(20) NOT NULL
 );
