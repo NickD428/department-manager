5ae00b039dbf7ba2e45efa140a00b4c88dc4bd3b
Nick <chargerfan428@aol.com>
2023-06-24 13:43:15 -0700

    added connection file for sql

---
+++ connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database.');
+});
+
+module.exports = connection;
