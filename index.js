8bc2f1e7af5a418f557e860f855b6b495c558fbc
Nick <chargerfan428@aol.com>
2023-06-24 13:54:19 -0700

    added answers switch function to index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const connection = require('./connection');
 
 inquirer
       .prompt({
@@ -16,3 +17,34 @@ inquirer
           'Exit',
         ],
       })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewDepartments();
+            break;
+          case 'View all roles':
+            viewRoles();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+          default:
+            console.log('Invalid choice. Please select a valid option.');
+            start();
+        }
+      });
