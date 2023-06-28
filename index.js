cad2b64bcd8189fbbd534f19596f8168c64c21d8
Nick <chargerfan428@aol.com>
2023-06-27 19:19:25 -0700

    added inquirer prompts

--- index.js
+++ index.js
@@ -6,11 +6,60 @@ const connection = mysql.createConnection({
   port: 3306,
   user: 'root',
   password: '',
-  database: 'department_manager',
+  database: 'department_manager_db',
 });
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to the database.');
-  startApp();
+  mainMenu();
 });
+
+function mainMenu() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+        ],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.action) {
+        case 'View all departments':
+          viewAllDepartments();
+          break;
+        case 'View all roles':
+          viewAllRoles();
+          break;
+        case 'View all employees':
+          viewAllEmployees();
+          break;
+        case 'Add a department':
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an employee role':
+          updateEmployeeRole();
+          break;
+        default:
+          console.log('Invalid choice.');
+          mainMenu();
+          break;
+      }
+    });
+}
